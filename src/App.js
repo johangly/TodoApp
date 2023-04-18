5835fe33e9e9ee2afad405faf7de06a45b08beda
johangly <johanglyfabiansucre@gmail.com>
2023-04-18 17:33:10 -0400

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">App
+    <div className="App">
+      <TodoCounter />
+      <h2>Has completado 2 de 3 TODO's</h2>
+      <TodoSearch />
+      <input placeholder='Cebolla'/>
+      <TodoList>
+        <TodoItem/>
+      </TodoList>
+      <CreateTodoButton />
     </div>
   );
 }
